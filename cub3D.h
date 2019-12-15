af23b4c15568d6ab6983dca176c309993ab468af
ylegzouli <ylegzoul@student.42.fr>
2019-12-15 19:27:59 +0100

    MAP header

--- cub3D.h
+++ cub3D.h
@@ -14,16 +14,16 @@
 # define CUB3D_H
 
 # include "./lib/libft.h"
+# include "./lib/gnl/get_next_line.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <math.h>
-# include "./lib/gnl/get_next_line.h"
+#include <mlx.h>
 
 # define LARGEUR_SCREEN 800
 # define HAUTEUR_SCREEN 600
 # define LARGEUR_CHAMP 60
-# define ANGLE_RAY (60/320) 
 # define SIZE_WALL 50
 
 //#  define K_ESC			65307
@@ -105,10 +105,6 @@ typedef struct			s_raycast
 	double	wall;
 }						t_raycast;
 
-/*-----------------------------------------------------------------------------------*/
-
-#include <mlx.h>
-
 typedef struct			s_mlx
 {
     void	*ptr;
@@ -121,40 +117,38 @@ typedef struct			s_mlx
 	int		en;
 }						t_mlx;
 
-void        init_mlx(t_mlx *mlx);
-void        affichage(t_mlx *mlx, t_raycast *ray, t_map *map, t_player *player);
-void        draw_column(t_mlx *mlx, t_raycast *ray, int x);
-void        put_pixel(t_mlx *mlx, int x, int y, int color);
-void        move_player(t_player *player, int key, t_map *map);
-void		rotate_player(t_player *player, int key, double angle);
-
-/*-----------------------------------------------------------------------------------*/
-
-typedef	struct		s_hook
+typedef	struct			s_hook
 {
 	t_mlx		*mlx;
 	t_raycast 	*ray;
 	t_map		*map;
 	t_player	*player;
-}					t_hook;
+}						t_hook;
 
-int     hook_keydown(int key, t_hook *hook);
-
-/*-----------------------------------------------------------------------------------*/
+/*----------------------------------- START ------------------------------------------*/
 
 void    	ft_print_debugage(/*t_parse *data, */t_map *map, t_player *player, t_raycast *ray);
+
 void        init_data(t_parse **data, t_map **map, t_player **player, t_raycast **ray, t_mlx **mlx, t_hook **hook);
+void        init_mlx(t_mlx *mlx);
+
+/*---------------------------------- PARSING -----------------------------------------*/
 
 void        parse_file(char **av, t_parse **data, t_map **map, t_player **player);
 void        parse_res(t_list *li, t_parse **data);
 void        parse_tex(t_list *li, t_parse **data);
 void        parse_color(t_list *li, t_parse **data);
 void        parse_map(t_list *li, t_map **map );
+void    	get_size_map(t_list *li, t_map **map);
 void        parse_player(t_map *map, t_player **player);
+void    	get_case(t_player **player, char tmp, int i, int j);
 
-void		get_ray_angle(int x, t_player *player, t_map *map, t_raycast *ray);
+/*----------------------------------- CHECK -------------------------------------------*/
+
+/*----------------------------------- RAYCAST -----------------------------------------*/
 
 void        raycast(t_map *map, t_player *player, t_raycast **ray);
+void		get_ray_angle(int x, t_player *player, t_map *map, t_raycast *ray);
 void		inter_x(t_map *map, t_player *player, t_raycast **ray);
 void        inter_x2(t_map *map, t_raycast **raycast);
 void        inter_y(t_map *map, t_player *player, t_raycast **ray);
@@ -163,10 +157,29 @@ void        distance_mur(t_player *player, t_raycast **ray);
 int			transfer_coords_x(t_map *map, double x);
 int			transfer_coords_y(t_map *map, double y);
 
+/*---------------------------------- AFFICHAGE ----------------------------------------*/
+
+void        affichage(t_mlx *mlx, t_raycast *ray, t_map *map, t_player *player);
+void        draw_column(t_mlx *mlx, t_raycast *ray, int x);
+
+/*----------------------------------- EVENT -------------------------------------------*/
+
+int			hook_keydown(int key, t_hook *hook);
+void        move_player(t_player *player, int key, t_map *map);
+void		rotate_player(t_player *player, int key, double angle);
+
+/*----------------------------------- IMAGE -------------------------------------------*/
+
+void        put_pixel(t_mlx *mlx, int x, int y, int color);
+
+/*----------------------------------- COLOR -------------------------------------------*/
+
+/*----------------------------------- OTHER -------------------------------------------*/
+
 int     	is_id(char *line);
 int    		is_map_line(char *line);
-void    	get_size_map(t_list *li, t_map **map);
-void    	get_case(t_player **player, char tmp, int i, int j);
 void		vect_mult(t_player *player);
+
+/*-------------------------------------------------------------------------------------*/
 
 #endif
